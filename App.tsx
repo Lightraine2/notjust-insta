d0f64f668d64be7516a30fddbe0e9b4583e2ed25
Laurence Dale <laurencedale1986@gmail.com>
2022-09-05 21:06:42 +0100

    starting post component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,34 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import colors from './src/theme/colors';
 import fonts from './src/theme/fonts';
+import {AntDesign} from '@expo/vector-icons';
+
+// get icons like so
+//import { createIconSet } from 'react-native-vector-icons';
 
 const App = () => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text style={{ color: colors.primary, fontSize: fonts.size.xl  }}>Hello World</Text>
+  <View style={styles.post}>
+    {/* Header */}
+
+    {/* Content */}
+    <Image source={{
+      uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg'
+      }} 
+      style={styles.image} 
+      />
+
+    {/* Footer */}
   </View>
 )
+
+const styles = StyleSheet.create({
+  post: {
+
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1
+  }
+})
 
 export default App;

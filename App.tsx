c9a88aa70be613eb63d70f5c0b285492d1c53b78
Laurence Dale <laurencedale1986@gmail.com>
2022-09-13 16:37:47 +0100

    to 3.2 custom component

--- App.tsx
+++ App.tsx
@@ -60,7 +60,36 @@ const App = () => (
   />
 </View>
 
-<Text>Liked by your mum and 66 others</Text>
+<Text style={styles.text}>
+  Liked by{' '} 
+  <Text style={styles.bold}>your mum</Text> and{' '} 
+  <Text style={styles.bold}>66 others</Text>
+  </Text>
+
+  {/* Post Description */}
+  <Text style={styles.text}>
+    <Text style={styles.bold}>vadimnotjustdev</Text>{' '}
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium eos earum quam quidem accusamus, provident dolores eaque temporibus! Quidem, nesciunt dicta doloremque assumenda quod consectetur dolore incidunt est perferendis iste!
+  </Text>
+
+  {/* Comments */}
+  <Text style={styles.lightText}>View all 16 comments</Text>
+  <View style={styles.comment}>
+    <Text style={styles.commentText}>
+      <Text style={styles.bold}>vadimnotjustdev</Text>{' '}
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, cum!
+    </Text>
+    <AntDesign
+    name={'hearto'}
+    size={16}
+    style={styles.icon}
+    color={colors.black}
+  />
+  </View>
+
+  {/* Posted date */}
+
+  <Text style={styles.lightText}>19th December 2021</Text>
 
 </View>
 </View>
@@ -101,6 +130,25 @@ const styles = StyleSheet.create({
   },
   icon: {
     marginHorizontal: 5
+  },
+  text: {
+    color: colors.black,
+    lineHeight: 18,
+  },
+  lightText: {
+    color: colors.lightgrey
+  },
+  bold: {
+    fontWeight: fonts.weight.bold
+  },
+  commentText: {
+    color: colors.black,
+    flex: 1,
+    lineHeight: 18,
+  },
+  comment: {
+    flexDirection: 'row',
+    alignItems: 'center',
   }
 })
 

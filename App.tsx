29711bea64c4fc4865ae7f0174a3865710bc784f
Laurence Dale <laurencedale1986@gmail.com>
2022-11-24 12:27:15 +0000

    to 3.12 react form hook

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import {View, StyleSheet} from 'react-native';
 import HomeScreen from './src/screens/HomeScreen/HomeScreen';
 import CommentsScreen from './src/screens/CommentsScreen/CommentsScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
+import EditProfileScreen from './src/screens/EditProfileScreen/EditProfileScreen';
 
 // get icons like so
 //import { createIconSet } from 'react-native-vector-icons';
@@ -14,7 +15,8 @@ const App = () => {
    
   {/* <HomeScreen /> */}
   {/* <CommentsScreen /> */}
-  <ProfileScreen />
+  {/* <ProfileScreen /> */}
+  <EditProfileScreen />
 
   </View>
 

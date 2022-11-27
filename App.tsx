a9cec1452d8d805f7c02c1bd5bcc8e05a5b1c359
Laurence Dale <laurencedale1986@gmail.com>
2022-11-27 11:16:15 +0000

    to post upload camera

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
+import React from 'react'
 import {View, StyleSheet} from 'react-native'; 
 import HomeScreen from './src/screens/HomeScreen/HomeScreen';
 import CommentsScreen from './src/screens/CommentsScreen/CommentsScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
 import EditProfileScreen from './src/screens/EditProfileScreen/EditProfileScreen';
+import PostUploadScreen from './src/screens/PostUploadScreen';
 
 // get icons like so
 //import { createIconSet } from 'react-native-vector-icons';
@@ -16,7 +18,8 @@ const App = () => {
   {/* <HomeScreen /> */}
   {/* <CommentsScreen /> */}
   {/* <ProfileScreen /> */}
-  <EditProfileScreen />
+  {/* <EditProfileScreen /> */}
+  <PostUploadScreen />
 
   </View>
 
---
+++ src/screens/PostUploadScreen/PostUploadScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const PostUploadScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>PostUploadScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default PostUploadScreen

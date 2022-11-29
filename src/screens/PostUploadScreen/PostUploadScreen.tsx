a85f01332f0406e49770c6f713f346d348c06727
Laurence Dale <laurencedale1986@gmail.com>
2022-11-29 13:01:11 +0000

    to 37:11

--- src/screens/PostUploadScreen/PostUploadScreen.tsx
+++ src/screens/PostUploadScreen/PostUploadScreen.tsx
@@ -1,11 +1,13 @@
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet, Pressable } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import { Camera } from 'expo-camera';
+import { Camera, FlashMode } from 'expo-camera';
 import colors from '../../theme/colors';
 import { MaterialIcons } from '@expo/vector-icons'; 
 
 const PostUploadScreen = () => {
   const [hasPermissions, setHasPermissions] = useState<boolean | null> (null);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
 
   useEffect(() => {
     const getPermission = async () => {
@@ -19,6 +21,16 @@ const PostUploadScreen = () => {
     getPermission();
   }, []);
 
+  const flipCamera = () => {
+    setCameraType(currentCameraType => 
+      currentCameraType === Camera.Constants.Type.back ? Camera.Constants.Type.front 
+      : Camera.Constants.Type.back );
+  }
+
+  const toggleFlash = () => {
+    console.log('toggleFlash')
+  }
+
   if (hasPermissions === null) {
     return <Text>Loading...</Text>
   }
@@ -30,11 +42,24 @@ const PostUploadScreen = () => {
 
   return (
     <SafeAreaView style={styles.page}>
-      <Camera style={styles.camera}/>
-      <View style={styles.buttonContainer}>
+      <Camera style={styles.camera} type={cameraType} ratio="4:3" flashMode={flash}/>
+      
+      <View style={[styles.buttonContainer, {top: 40}]}>
         <MaterialIcons name="close" size={30} color={colors.white} />
-        <Text style={{color: colors.white}}>boogers live here</Text>
+        <Pressable onPress={toggleFlash}>
+        <MaterialIcons name="flash-off" size={30} color={colors.white} />
+        </Pressable>
+        <MaterialIcons name="settings" size={30} color={colors.white} />
       </View>
+      
+      <View style={[styles.buttonContainer, {bottom: 25}]}>
+        <MaterialIcons name="photo-library" size={30} color={colors.white} />
+        <View style={styles.circle}/>
+        <Pressable onPress={flipCamera}>
+        <MaterialIcons name="flip-camera-ios" size={30} color={colors.white} />
+        </Pressable>
+      </View>
+    
     </SafeAreaView>
   )
 }
@@ -52,9 +77,18 @@ const styles = StyleSheet.create({
 
     },
     buttonContainer: {
-        position: 'absolute',
-        marginLeft: 100
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      width: '100%',
+      position: 'absolute'
 
+    },
+    circle: {
+      width: 75,
+      aspectRatio: 1,
+      borderRadius: 75,
+      backgroundColor: colors.white
     }
 })
 

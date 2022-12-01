0629eb33ac7dcd102e064bdba6066417b897aff1
Laurence Dale <laurencedale1986@gmail.com>
2022-12-01 15:06:06 +0000

    to 4.3 moving between screens

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,13 +1,54 @@
 import React from 'react'
+import {View, Text, Image } from 'react-native'
 import {NavigationContainer} from '@react-navigation/native'
 import HomeScreen from '../screens/HomeScreen/HomeScreen'
+import ProfileScreen from '../screens/ProfileScreen'
+import logo from '../assets/images/logo.png'
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <HomeScreen />
+      <Stack.Navigator 
+      initialRouteName='Feed' 
+      screenOptions={{headerShown: true}}>
+        <Stack.Screen 
+        name="Feed" 
+        component={HomeScreen} 
+        //custom header
+//        options={{header: Header}}
+
+        options={{headerTitle: HeaderTitle}}
+        />
+        <Stack.Screen 
+        name="UserProfile" 
+        component={ProfileScreen} 
+        options={{title: 'Profile'}}
+        />
+
+      </Stack.Navigator>
     </NavigationContainer>
   )
+}
+
+//custom header component
+// const Header = () => {
+//   return (
+//     <View>
+//       <Text>Custom Header</Text>
+//     </View>
+//   )
+// }
+
+const HeaderTitle = () => {
+
+  return (
+    <Image source={logo} resizeMode="contain" style={{width: 150, height: 40}} />
+  )
+
 }
 
 export default Navigation;

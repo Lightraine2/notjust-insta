064c67be1bf03381e089d8f430e85da90e1e1620
Laurence Dale <laurencedale1986@gmail.com>
2022-12-01 12:15:59 +0000

    to 4.2 stack navigator

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import {View, StyleSheet} from 'react-native'; 
-import HomeScreen from './src/screens/HomeScreen/HomeScreen';
-import CommentsScreen from './src/screens/CommentsScreen/CommentsScreen';
-import ProfileScreen from './src/screens/ProfileScreen';
-import EditProfileScreen from './src/screens/EditProfileScreen/EditProfileScreen';
-import PostUploadScreen from './src/screens/PostUploadScreen';
+// import HomeScreen from './src/screens/HomeScreen/HomeScreen';
+// import CommentsScreen from './src/screens/CommentsScreen/CommentsScreen';
+// import ProfileScreen from './src/screens/ProfileScreen';
+// import EditProfileScreen from './src/screens/EditProfileScreen/EditProfileScreen';
+// import PostUploadScreen from './src/screens/PostUploadScreen';
+
+
+import Navigation from './src/navigation';
 
 // get icons like so
 //import { createIconSet } from 'react-native-vector-icons';
@@ -13,24 +16,11 @@ import PostUploadScreen from './src/screens/PostUploadScreen';
 const App = () => {
   return (
 
-  <View style={styles.app}>
-   
-  {/* <HomeScreen /> */}
-  {/* <CommentsScreen /> */}
-  {/* <ProfileScreen /> */}
-  {/* <EditProfileScreen /> */}
-  <PostUploadScreen />
-
-  </View>
+    <Navigation />
 
   );
 }
 
 
-const styles = StyleSheet.create({
-  app: {
-    flex: 1,
-  }
-})
 
 export default App;

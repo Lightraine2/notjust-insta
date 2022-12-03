b4966170735b8dabbb44853e25cfbaa46dee8678
Laurence Dale <laurencedale1986@gmail.com>
2022-12-03 20:55:57 +0000

    to 4.5

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -5,17 +5,22 @@ import ProfileScreen from '../screens/ProfileScreen';
 import PostUploadScreen from '../screens/PostUploadScreen';
 import { MaterialIcons } from '@expo/vector-icons'; 
 import colors from '../theme/colors';
-
+import HomeStackNavigator from './HomeStackNavigator';
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
     return (
-        <Tab.Navigator screenOptions={{tabBarShowLabel: false, tabBarActiveTintColor: colors.primary}}>
+        <Tab.Navigator screenOptions={{
+            tabBarShowLabel: false, 
+            tabBarActiveTintColor: colors.black
+            }}>
             <Tab.Screen 
-            name='Feed' 
-            component={HomeScreen} 
-            options={{tabBarIcon: ({color, size}) => 
+            name='HomeStack' 
+            component={HomeStackNavigator} 
+            options={{
+                headerShown: false,
+                tabBarIcon: ({color, size}) => 
             <MaterialIcons name="home-filled"
             size={size}
             color={color}
@@ -35,14 +40,32 @@ const BottomTabNavigator = () => {
             <Tab.Screen 
             name='Upload' 
             component={PostUploadScreen}
+            options={{
+                headerShown: false,
+                tabBarIcon: ({color, size}) =>
+            <MaterialIcons name="add-circle"
+            size={size}
+            color={color}
+            />
+            }} />
+            <Tab.Screen 
+            name='Notifications' 
+            component={PostUploadScreen} 
+            options={{tabBarIcon: ({color, size}) =>
+            <MaterialIcons name="add-circle"
+            size={size}
+            color={color}
+            />
+            }}/>
+            <Tab.Screen 
+            name='MyProfile' 
+            component={ProfileScreen}
             options={{tabBarIcon: ({color, size}) =>
             <MaterialIcons name="add-circle"
             size={size}
             color={color}
             />
             }} />
-            <Tab.Screen name='Notifications' component={PostUploadScreen} />
-            <Tab.Screen name='MyProfile' component={ProfileScreen} />
         </Tab.Navigator>
     )
 }
